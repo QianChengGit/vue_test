60d4811c38c7b7618ebcf5a7f21f88c6bf4137c3
QianChengGit <2483184985@qq.com>
2021-08-31 21:50:13 +0800

    修改脚手架默认配置

---
+++ src/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名字：{{schoolName}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我提示</button>
+    </div> 
+</template>
+
+<script>
+//组件交互相关的代码（数据、方法等等）
+//export default Vue.extend({})可以简写为：export default {}
+    export default {
+        name:'School',
+        data(){
+            return {
+                schoolName:'电信学院',
+                address:'北京'
+            }
+        },
+        methods: {
+            showName(){
+                console.log(this.schoolName)
+            }
+        },
+    }
+</script>
+
+    
+<style>
+/* 组件的样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <h2>学生名字：{{studentName}}</h2>
+        <h2>年龄：{{age}}</h2>
+    </div> 
+</template>
+
+<script>
+//组件交互相关的代码（数据、方法等等）
+//export default Vue.extend({})可以简写为：export default {}
+    export default {
+        name:'Student',
+        data(){
+            return {
+                studentName:'Tom',
+                age:22
+            }
+        },
+    }
+</script>
