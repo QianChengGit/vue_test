34e5221d8456012d7b2358c8e89a02c23ce1773f
QianChengGit <2483184985@qq.com>
2021-09-15 18:18:38 +0800

    mixin混入（合）

--- src/components/School.vue
+++ src/components/School.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- 组件的结构 -->
     <div>
-        <h2 @click="showName">学生姓名：{{Name}}</h2>
-        <h2>学生性别：{{sex}}</h2>
+        <h2 @click="showName">学校名称：{{Name}}</h2>
+        <h2>学校地址：{{address}}</h2>
         <h2>坐标x：{{x}}，坐标y：{{y}}</h2>
     </div> 
 </template>
@@ -13,16 +13,15 @@
     //组件交互相关的代码（数据、方法等等）
     //export default Vue.extend({})可以简写为：export default {}
     export default {
-        name:'Student',
+        name:'School',
         data(){
             return {
-                Name:'张三',
-                sex:'男'
+                Name:'电信学院',
+                address:'成都',
+                x:600,
+                y:500,
             }
         },
-        mixins:[mixins01,mixins02],//注册使用混合
-        mounted() {
-            console.log("你好啊！！！");
-        },
+        mixins:[mixins01,mixins02]//注册使用混合
     }
 </script>
