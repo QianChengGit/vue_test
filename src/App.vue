11a3bd0656585c7749da7319ee6508d6d81663f0
QianChengGit <2483184985@qq.com>
2022-01-05 11:36:20 +0800

    组件自定义事件_解绑

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}，学生姓名是：{{ studentName }}</h1>
     <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
     <School :getSchoolName="getSchoolName"></School>
     <!-- 通过父组件给子组件绑定一个事件实现：子给父传递数据(第一种写法，使用@或v-on) -->
-    <!-- <Student v-on:atguigu="getStudentName"></Student> -->
+    <!-- <Student v-on:atguigu="getStudentName" @demo="m1"></Student> -->
     <!-- 通过父组件给子组件绑定一个事件实现：子给父传递数据(第二种写法，使用ref) -->
-    <Student ref="student"></Student>
+    <Student ref="student" @click.native="show"></Student>
   </div>
 </template>
 
@@ -22,18 +22,38 @@ export default {
   data() {
     return {
       msg: "你好啊！",
+      studentName: "",
     };
   },
   methods: {
     getSchoolName(name) {
       console.log("App收到了学校名：", name);
     },
-    getStudentName(name) {
+    /*getStudentName(name) {
       console.log("App收到了学生名：", name);
+      this.studentName = name;
+    },*/
+    m1() {
+      console.log("demo事件被触发！");
+    },
+    show() {
+      alert("student组件触发原生点击事件");
     },
   },
   mounted() {
-    this.$refs.student.$on("atguigu", this.getStudentName);
+    //  this.$refs.student.$on("atguigu", this.getStudentName);
+
+    /*this.$refs.student.$on("atguigu", function (name) {
+      console.log("App收到了学生名：", name);
+      console.log(this);
+      this.studentName = name;
+    });*/
+
+    this.$refs.student.$on("atguigu", (name) => {
+      console.log("App收到了学生名：", name);
+      console.log(this);
+      this.studentName = name;
+    });
   },
 };
 </script>
--- src/components/Student.vue
+++ src/components/Student.vue
@@ -3,7 +3,11 @@
   <div class="student">
     <h2>学生姓名：{{ Name }}</h2>
     <h2>学生性别：{{ sex }}</h2>
+    <h2>当前求和为：{{ number }}</h2>
+    <button @click="add">点我number++</button>
     <button @click="sendStudentName">把学生名字给App</button>
+    <button @click="unbind">解绑atguigu事件</button>
+    <button @click="death">销毁当前Student组件的实例对象（VC）</button>
   </div>
 </template>
 
@@ -14,11 +18,30 @@ export default {
     return {
       Name: "张三",
       sex: "男",
+      number: 0,
     };
   },
   methods: {
+    add() {
+      console.log("add回调被调用了");
+      this.number++;
+    },
     sendStudentName() {
+      //触发Student组件实例对象身上的atguigu事件
       this.$emit("atguigu", this.Name);
+      //触发Student组件实例对象身上的m1事件
+      // this.$emit("demo");
+    },
+    unbind() {
+      //解绑一个自定义事件
+      this.$off("atguigu");
+      //解绑多个自定义事件
+      // this.$off(["atguigu", "demo"]);
+      //解绑所有的自定义事件
+      // this.$off();
+    },
+    death() {
+      this.$destroy(); //销毁了当前组件的实例,销毁后所有Student实例的自定义事件全都不奏效。
     },
   },
 };
