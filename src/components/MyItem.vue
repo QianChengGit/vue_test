7aefaf2e1466b16e8210edc1adece8c626041820
QianChengGit <2483184985@qq.com>
2022-01-28 17:47:04 +0800

    TodoList案例的消息订阅与发布

---
+++ src/components/MyItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <li>
+    <label>
+      <input
+        type="checkbox"
+        :checked="todo.done"
+        @change="handleCheck(todo.id)"
+      />
+      <!-- 如下代码也能实现功能，但不建议使用v-model去双向绑定props里声明接收的对象，因为有点违反原则，因为修改了props。 -->
+      <!-- <input type="checkbox" v-model="todo.done" /> -->
+      <span>{{ todo.title }}</span>
+    </label>
+    <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+  </li>
+</template>
+
+<script>
+import pubsub from "pubsub-js";
+export default {
+  name: "MyItem",
+  // 接收todo对象数据
+  props: ["todo"],
+  methods: {
+    handleCheck(id) {
+      //通知App组件将对应的todo对象的done值取反
+      // this.checkTodo(id);
+      this.$bus.$emit("checkTodo", id);
+    },
+    handleDelete(id) {
+      if (confirm("确定要删除吗？")) {
+        // this.deleteTodo(id);
+        // this.$bus.$emit("deleteTodo", id);
+        // 发布消息
+        pubsub.publish("deleteTodo", id);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+li label {
+  float: left;
+  cursor: pointer;
+}
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+li:before {
+  content: initial;
+}
+li:last-child {
+  border-bottom: none;
+}
+li:hover {
+  background: #ddd;
+}
+li:hover button {
+  display: block;
+}
+</style>
