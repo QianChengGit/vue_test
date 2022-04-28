97798f05c72cf8e5e2755a8aa8dbe209b9a6fe58
QianChengGit <2483184985@qq.com>
2022-04-28 19:54:34 +0800

    默认插槽

---
+++ 23_插槽/1_src_默认插槽/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <ul>
+      <!-- 定义插槽（挖个坑，等着组件使用者进行填充），vue会将组件里的img标签填入这里 -->
+      <slot>我是默认值，当使用者没有传递具体结构时，我会出现</slot>
+      <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["listData", "title"],
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
