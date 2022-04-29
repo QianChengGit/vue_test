90fa33327e89e000f40a88627fdaa01615f8e050
QianChengGit <2483184985@qq.com>
2022-04-29 20:28:05 +0800

    默认插槽、具名插槽、作用域插槽

---
+++ 23_插槽/2_src_具名插槽/components/Category.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <!-- 定义具体名字的插槽（挖个坑，等着组件使用者进行填充），vue会将组件里的img标签填入这里 -->
+    <slot name="center">我是默认值，当使用者没有传递具体结构时，我会出现1</slot>
+    <ul>
+      <li v-for="(item, index) in listData" :key="index">{{ item }}</li>
+    </ul>
+    <slot name="footer">我是默认值，当使用者没有传递具体结构时，我会出现2</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ["listData", "title"],
+};
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 400px;
+}
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
