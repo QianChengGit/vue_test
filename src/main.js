fd2822eca5eeba3440705488ffad466027a0cae5
QianChengGit <2483184985@qq.com>
2022-05-05 19:40:43 +0800

    求和案例_Vuex版

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入vue-resource插件
 import vueResource from 'vue-resource'
-Vue.config.productionTip = false
+// 引入store
+import store from './store/index'
 
+// 关闭Vue的生产提示
+Vue.config.productionTip = false
 //使用插件
 Vue.use(vueResource)
 
+// 创建vm
 new Vue({
     el: '#app',
     render: h => h(App),
+    store,
     beforeCreate() {
         //安装全局事件总线
         Vue.prototype.$bus = this
