3e8d7870fb0fbc23d80668efddba64795d201a1c
QianChengGit <2483184985@qq.com>
2022-07-19 21:02:32 +0800

    路由的基本使用

--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
+// 引入vue
 import Vue from 'vue'
+// 引入App
 import App from './App.vue'
-//引入vue-resource插件
-import vueResource from 'vue-resource'
-// 引入store
-import store from './store/index'
+// 引入vue-router
+import VueRouter from 'vue-router'
+// 引入路由器
+import router from './router'
 
 // 关闭Vue的生产提示
 Vue.config.productionTip = false
-//使用插件
-Vue.use(vueResource)
+// 应用VueRouter插件
+Vue.use(VueRouter)
 
 // 创建vm
 new Vue({
     el: '#app',
     render: h => h(App),
-    store,
-    beforeCreate() {
-        //安装全局事件总线
-        Vue.prototype.$bus = this
-    },
+    router: router,
 })
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+// 该文件用于创建整个应用的路由器
+
+// 引入VueRouter插件
+import VueRouter from 'vue-router'
+// 引入自定义组件
+import About from '../components/About.vue'
+import Home from '../components/Home.vue'
+
+// 创建一个路由器
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About,
+        },
+        {
+            path: '/home',
+            component: Home,
+        },]
+})
+
+// 导出路由
+export default router
