0a9c48bd705e0c0a9b64bc1e6efebb7354e3de30
QianChengGit <2483184985@qq.com>
2022-08-07 11:41:00 +0800

    路由使用的几个注意点

---
+++ 32_src_路由使用的几个注意点/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>我是Home的内容</h2>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  /*beforeDestroy() {
+    console.log("Home组件即将被销毁！");
+  },*/
+  mounted() {
+    console.log("Home组件挂载完毕！", this);
+    window.HomeRoute = this.$route;
+    window.HomeRouter = this.$router;
+  },
+};
+</script>
